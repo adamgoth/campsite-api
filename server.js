67e58f86b77ec8e44ff1377d4735f349cfe7f040
Adam Goth <apg@Adams-MacBook-Pro.local>
2016-04-23 16:44:40 -0600

    Adds find by state to API

--- server.js
+++ server.js
@@ -13,12 +13,25 @@ mongoose.connect('mongodb://apg:apg@ds023468.mlab.com:23468/apgtestdb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended": false}));
 
-app.get('/campsites', function(req, res) {
+var router = express.Router();
+
+//get all
+router.get('/campsites', function(req, res) {
   Campsite.find({}, function(err, campsite) {
     if (err) throw err;
     res.json(campsite);
   });
 });
+
+//get by state
+router.get('/campsites/:state', function(req, res) {
+  Campsite.find({state: req.params.state}, function(err, campsite) {
+    if (err) throw err;
+    res.json(campsite);
+  });
+});
+
+app.use('/api', router);
 
 app.listen(3000);
 console.log("Listening to PORT 3000");
