214d70eb167cbdc17be1710bfe39b79b169e2877
Adam Goth <adamgoth@gmail.com>
2016-05-06 10:54:57 -0600

    Adds routes for reviews

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ var mongoose = require('mongoose');
 var campsiteSchema = new mongoose.Schema({
   campsite: String,
 });
+var reviewSchema = new mongoose.Schema({
+  user: String,
+  review: String,
+  rating: Number,
+  campsite_id: String
+});
 var Campsite = mongoose.model('Campsite', campsiteSchema);
+var Review = mongoose.model('Review', reviewSchema);
+
 mongoose.connect('mongodb://apg:apg@ds023468.mlab.com:23468/apgtestdb');
 
 var port = process.env.PORT || 3000;
@@ -46,6 +54,36 @@ app.get('/campsites/:id', function(req, res) {
   Campsite.findById(req.params.id, function(err, campsite) {
     if (err) throw err;
     res.json(campsite);
+  });
+});
+
+//get all reviews
+app.get('/reviews', function(req, res) {
+  Review.find({}, function(err, review) {
+    if (err) throw err;
+    res.json(review);
+  });
+});
+
+//post review
+app.post('/reviews', function(req, res) {
+  var review = new Review();
+  review.user = req.body.user;
+  review.review = req.body.review;
+  review.rating = req.body.rating;
+  review.campsite_id = req.body.campsite_id;
+
+  review.save(function(err) {
+    if (err) throw err;
+    res.json({message: 'Review saved!'});
+  });
+});
+
+//get reviews by campsite_id
+app.get('/reviews/:campsite_id', function(req, res) {
+  Review.find({campsite_id: req.params.campsite_id}, function(err, review) {
+    if (err) throw err;
+    res.json(review);
   });
 });
 
